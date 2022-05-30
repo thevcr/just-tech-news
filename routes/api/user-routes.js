3c2921680975d49ea482e78e79b3aa376ebaca29
thevcr <thevcr@gmail.com>
2022-05-30 07:43:32 -0400

    add login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,7 @@ const { User } = require("../../models");
 router.get("/", (req, res) => {
   // Access our User model and run .findAll() method)
   User.findAll({
-      attributes: { exclude: ['password'] }
+    attributes: { exclude: ["password"] },
   })
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
@@ -17,7 +17,7 @@ router.get("/", (req, res) => {
 // GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
-    attributes: { exclude: ['password'] },  
+    attributes: { exclude: ["password"] },
     where: {
       id: req.params.id,
     },
@@ -33,6 +33,29 @@ router.get("/:id", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+router.post("/login", (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+
+    // Verify user
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
 });
 
 // POST /api/users
@@ -59,7 +82,7 @@ router.put("/:id", (req, res) => {
     individualHooks: true,
     where: {
       id: req.params.id,
-    }
+    },
   })
     .then((dbUserData) => {
       if (!dbUserData[0]) {
